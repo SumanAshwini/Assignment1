f83252cc31531bd5b87cbab6b7140a9512b0b399
alinannthomas <alinannthomas1991@gmail.com>
2019-12-06 15:39:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/apple/assignment1/general/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.apple.assignment1.general;
+
+public class AppConstants {
+
+    String domain = "https://dl.dropboxusercontent.com/";
+}
---
+++ app/src/main/java/com/example/apple/assignment1/main_activity/model/MTitle.java
@@ -0,0 +1,69 @@
+package com.example.apple.assignment1.main_activity.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class MTitle {
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("rows")
+    @Expose
+    private ArrayList<Row> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Row> getRows() {
+        return rows;
+    }
+
+    public void setRows(ArrayList<Row> rows) {
+        this.rows = rows;
+    }
+
+    public class Row {
+
+        @SerializedName("title")
+        @Expose
+        private String title;
+        @SerializedName("description")
+        @Expose
+        private String description;
+        @SerializedName("imageHref")
+        @Expose
+        private String imageHref;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getImageHref() {
+            return imageHref;
+        }
+
+        public void setImageHref(String imageHref) {
+            this.imageHref = imageHref;
+        }
+
+    }
+}
